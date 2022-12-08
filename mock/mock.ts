2ee7e3c494e5857b753868d5cf6ecfb53b4f5c13
ipqw <iga538741@gmail.com>
2022-12-08 19:01:02 +0400

    add mock

---
+++ mock/mock.ts
@@ -0,0 +1,80 @@
+export const developers = [
+    {
+        name: 'Губарев Владислав',
+        id: 1,
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        address: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        technologies: 'HTML, CSS, TypeScript, React'
+    },
+    {
+        name: 'Иванов Герман',
+        id: 2,
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        address: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        technologies: 'HTML, CSS, TypeScript, React'
+    },
+    {
+        name: 'Овчинников Владимир',
+        id: 3,
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        address: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        technologies: 'HTML, CSS, TypeScript, React'
+    },
+    {
+        name: 'Гладенко Илья',
+        id: 4,
+        description: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        address: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+        technologies: 'HTML, CSS, TypeScript, React'
+    }
+]
+export const projects = [
+    {
+        name: 'Интернет-магазин',
+        author: 'Губарев Владислав',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+    {
+        name: 'Сайт визитка',
+        author: 'Губарев Владислав',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+    {
+        name: 'Интернет-магазин',
+        author: 'Иванов Герман',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+    {
+        name: 'Сайт визитка',
+        author: 'Иванов Герман',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+    {
+        name: 'Интернет-магазин',
+        author: 'Овчинников Владимир',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+    {
+        name: 'Сайт визитка',
+        author: 'Овчинников Владимир',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+    {
+        name: 'Интернет-магазин',
+        author: 'Гладенко Илья',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+    {
+        name: 'Сайт визитка',
+        author: 'Гладенко Илья',
+        target: 'Личный проект',
+        description: 'Краткое описание проекта. Lorem ipsum dolor sit amet consectetur. Senectus sed velit libero urna sem vivamus senectus penatibus turpis. Ut enim neque malesuada quam velit diam ipsum sagittis nec. Ut consequat tempus ullamcorper vivamus. Amet neque aenean gravida maecenas.',
+    },
+]
